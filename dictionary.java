8d2038921d5d809731b9491e6d941edec0a53cb4
Michael Ryan <mike.ryan871@gmail.com>
2020-03-06 22:26:02 -0500

    Create/Display a JFrame.

--- dictionary.java
+++ dictionary.java
@@ -2,11 +2,17 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-  public class Dictionary {
+  public class dictionary {
 
     public static void createAndShowGUI(){
+      // Create(s) the window
+      JFrame frame = new JFrame("Dictionary");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
+      // Display(s) the window
+      frame.setPreferredSize(new Dimension(900, 750));
+      frame.pack();
+      frame.setLocationRelativeTo(null);
 
     }
   }
