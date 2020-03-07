ff71b6645d978040d26bb8021f405543803135ac
Michael Ryan <mike.ryan871@gmail.com>
2020-03-06 22:31:38 -0500

    Added 'Add' and 'Remove' Button(s) to JFrame.

--- dictionary.java
+++ dictionary.java
@@ -14,5 +14,16 @@ import java.awt.event.*;
       frame.pack();
       frame.setLocationRelativeTo(null);
 
+      // Add(s) "Add" Button
+      JButton add = new JButton("Add");
+      add.setBounds(5, 5, 100, 35);
+      frame.add(add);
+
+      // Add(s) "Remove" Button
+      JButton remove = new JButton("Remove");
+      remove.setBounds(110, 5, 100, 35);
+      frame.add(remove);
+
+      
     }
   }
